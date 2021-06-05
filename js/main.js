0235d10ebeb37fa529e4c72f8bf30d3b77f38aa4
Павел <pavel.tagiltsef@gmail.com>
2021-06-05 22:57:55 +0300

    Добавляет скрипт с анимацией

---
+++ js/main.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+    
+    const animOnScroll = function()  {
+        
+        const offset = function(el) {
+            const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return {top: rect.top + scrollTop, left: rect.left + scrollLeft};
+        };
+
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight  = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+            
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+            
+            if ((window.pageYOffset > animItemOffset - animItemPoint) &&  window.pageYOffset < (animItemOffset + animItemPoint)) {
+                console.log(animItemPoint);
+                animItem.classList.add('_active');
+            } else {
+                if (!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+        
+    };
+    
+    window.addEventListener('scroll', animOnScroll);
+    animOnScroll();
+    
+}
