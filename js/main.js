5491cbf9e0e3475d695d552ddc99e09b274273b5
Павел <pavel.tagiltsef@gmail.com>
2021-06-07 20:38:39 +0300

    Рефакторит js код и улучшает логику

--- js/main.js
+++ js/main.js
@@ -1,78 +1,138 @@
 "use strict";
 
-const animItems = document.querySelectorAll('._anim-items');
+window.addEventListener("DOMContentLoaded", () => {
 
-if (animItems.length > 0) {
+    // Animation logic
+
+    const animItems = document.querySelectorAll('._anim-items');
     
-    const animOnScroll = function()  {
+    if (animItems.length > 0) {
         
-        const offset = function(el) {
-            const rect = el.getBoundingClientRect(),
-            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-            return {top: rect.top + scrollTop, left: rect.left + scrollLeft};
-        };
-
-        for (let index = 0; index < animItems.length; index++) {
-            const animItem = animItems[index];
-            const animItemHeight  = animItem.offsetHeight;
-            const animItemOffset = offset(animItem).top;
-            const animStart = 4;
+        const animOnScroll = function()  {
             
-            let animItemPoint = window.innerHeight - animItemHeight / animStart;
-            if (animItemHeight > window.innerHeight) {
-                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            const offset = function(el) {
+                const rect = el.getBoundingClientRect(),
+                scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+                scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+                return {top: rect.top + scrollTop, left: rect.left + scrollLeft};
+            };
+    
+            for (let index = 0; index < animItems.length; index++) {
+                const animItem = animItems[index];
+                const animItemHeight  = animItem.offsetHeight;
+                const animItemOffset = offset(animItem).top;
+                const animStart = 4;
+                
+                let animItemPoint = window.innerHeight - animItemHeight / animStart;
+                if (animItemHeight > window.innerHeight) {
+                    animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                }
+                
+                if ((window.pageYOffset > animItemOffset - animItemPoint) &&  
+                window.pageYOffset < (animItemOffset + animItemPoint)) {
+                    console.log(animItemPoint);
+                    animItem.classList.add('_active');
+                } else {
+                    if (!animItem.classList.contains('_anim-no-hide')) {
+                        animItem.classList.remove('_active');
+                    }
+                }
             }
             
-            if ((window.pageYOffset > animItemOffset - animItemPoint) &&  
-            window.pageYOffset < (animItemOffset + animItemPoint)) {
-                console.log(animItemPoint);
-                animItem.classList.add('_active');
+        };
+        
+        window.addEventListener('scroll', animOnScroll);
+        animOnScroll();
+        
+    }
+
+    // Button, which leads up, logic
+
+    let btn = document.querySelector("#btn");
+    
+    btn.onclick = function() {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    };
+    
+    function showUpButton() {
+        if ((window.pageYOffset + document.documentElement.clientHeight) + 1000 >= 
+        document.documentElement.scrollHeight) {
+            btn.classList.add("fixed"); 
+        } else {
+            btn.classList.remove("fixed");
+        }
+    }
+    
+    window.addEventListener('scroll', showUpButton);
+    
+
+    // Header logic
+
+    function headerScrollAnimation() {
+        const header = document.querySelector('.main-header');
+
+        function hideHeader() {
+            if (window.pageYOffset > header.offsetHeight) {
+                header.classList.add('header-hide');
             } else {
-                if (!animItem.classList.contains('_anim-no-hide')) {
-                    animItem.classList.remove('_active');
-                }
+                header.classList.remove('header-hide');
             }
         }
         
-    };
-    
-    window.addEventListener('scroll', animOnScroll);
-    animOnScroll();
-    
-}
+        function fixedHeader() {
+            if (window.pageYOffset > header.offsetHeight * 2) {
+                header.classList.add('header-fixed');
+                document.body.style.marginTop = `${header.clientHeight}px`;
+            } else {
+                header.classList.remove('header-fixed');
+                document.body.style.marginTop = '0px';
+            }  
+        }
 
-let btn = document.querySelector("#btn");
-
-btn.onclick = function() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth',
-  });
-};
-
-function showModalByScroll() {
-    if ((window.pageYOffset + document.documentElement.clientHeight) + 1000 >= 
-    document.documentElement.scrollHeight) {
-        btn.classList.add("fixed"); 
-    } else {
-        btn.classList.remove("fixed");
+        hideHeader();
+        fixedHeader();
     }
-}
 
-window.addEventListener('scroll', showModalByScroll);
+    headerScrollAnimation();
+    window.addEventListener('scroll', headerScrollAnimation);
 
+    // Slider logic
 
-const header = document.querySelector('.main-header');
-
-function fixedHeader() {
-    if (window.pageYOffset > window.pageYOffset - header.offsetHeight) {
-        header.classList.add('header-fixed');
-        document.body.style.marginTop = `${header.clientHeight}px`;
-    } else {
-        header.classList.remove('header-fixed');
-        document.body.style.marginTop = '0px';
+    if (window.matchMedia("(max-width: 540px)").matches) {
+        // ... ваша логика
+        // которая не станет выполняться
+        // если размер больше 540px
     }
-}
-fixedHeader();
-window.addEventListener('scroll', fixedHeader);
+    
+    const swiper = new Swiper('.swiper-container', {
+        speed: 400,
+        // spaceBetween: 100,
+        direction: 'horizontal',
+        slidesPerView: 3,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 2,
+              spaceBetween: 25
+            },
+            // when window width is >= 480px
+            678: {
+              slidesPerView: 2,
+              spaceBetween: 50
+            },
+            // when window width is >= 640px
+            1300: {
+              slidesPerView: 3,
+              spaceBetween: 100
+            }
+          }
+      });
+
+});
