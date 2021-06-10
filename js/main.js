f7523fd1254acb97fbeba1aa5797e9a30740e2fa
Павел <pavel.tagiltsef@gmail.com>
2021-06-10 18:52:12 +0300

    Вносит мелкие улучшения

--- js/main.js
+++ js/main.js
@@ -114,7 +114,7 @@ window.addEventListener("DOMContentLoaded", () => {
         slidesPerView: 3,
         navigation: {
             nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            prevEl: '.swiper-button-prev'
         },
         breakpoints: {
             // when window width is >= 320px
