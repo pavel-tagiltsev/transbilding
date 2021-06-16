46a1279c8fff9fb189b5674b3b42a0fba0aef51a
Павел <pavel.tagiltsef@gmail.com>
2021-06-16 16:32:30 +0300

    Добавляет адаптив шапке (черновик) и вносит мелкие правки в другие разделы

--- js/main.js
+++ js/main.js
@@ -212,10 +212,43 @@ window.addEventListener("DOMContentLoaded", () => {
         //     }
         // }
     });
+
+    const button = document.querySelector(".main-nav__toggle");
+    const list = document.querySelector(".main-nav__list");
+
+    // button.classList.remove("no--js");
+
+    window.addEventListener('resize', () => {
+        
+        if (window.matchMedia("(min-width: 1180px)").matches) {
+            list.classList.remove("main-nav--hidden");
+        }
+        if (window.matchMedia("(max-width: 1180px)").matches) {
+            list.classList.add("main-nav--hidden");
+        }
+    });
+
+    if (window.matchMedia("(max-width: 1180px)").matches) {
+        list.classList.add("main-nav--hidden");
+    }
+    
+
+    button.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        list.classList.toggle("main-nav--hidden");
+        if (button.classList.contains("main-nav__toggle--opened")) {
+            button.classList.remove("main-nav__toggle--opened");
+            button.classList.add("main-nav__toggle--closed");
+        } else {
+            button.classList.remove("main-nav__toggle--closed");
+            button.classList.add("main-nav__toggle--opened");
+        }
+    });
+
 });
 
-   // if (window.matchMedia("(max-width: 540px)").matches) {
-        // ... ваша логика
-        // которая не станет выполняться
-        // если размер больше 540px
-    // }
+//    if (window.matchMedia("(max-width: 1180px)").matches) {
+//         ... ваша логика
+//         которая не станет выполняться
+//         если размер больше 540px
+//     }
