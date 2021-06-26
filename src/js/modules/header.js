acf7695c5305eb8cb34e3707a5df509598b2a340
Павел <pavel.tagiltsef@gmail.com>
2021-06-26 23:40:31 +0300

    Фиксит баг с шапкой

--- src/js/modules/header.js
+++ src/js/modules/header.js
@@ -1,31 +1,40 @@
 'use strict';
 
 function fixedHeader() {
-    let oldScrollY = 0;
-    const header = document.querySelector('.main-header');
+  let oldScrollY = 0;
+  const header = document.querySelector('.main-header');
 
-    window.addEventListener('scroll', function () {
+  window.addEventListener('scroll', function () {
 
-        function hideHeader() {
-            if (window.pageYOffset > header.offsetHeight) {
-                header.classList.add('header-hide');
-            } else {
-                header.classList.remove('header-hide');
-            }
-        }
+    function hideHeader() {
+      if (window.pageYOffset > header.offsetHeight) {
+        header.classList.add('header-hide');
+      } else {
+        header.classList.remove('header-hide');
+      }
 
-        let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-        let dY = scrolled - oldScrollY;
+      const button = document.querySelector(".main-nav__toggle");
+      const list = document.querySelector(".main-nav__list");
 
-        if (dY < 0) {
-            header.classList.add('header-fixed');
-        } else {
-            header.classList.remove('header-fixed');
-        }
+      list.classList.add("main-nav__list--hidden");
+      if (button.classList.contains('main-nav__toggle--opened')) {
+        button.classList.add("main-nav__toggle--closed");
+        button.classList.remove("main-nav__toggle--opened");
+      }
+    }
 
-        hideHeader();
-        oldScrollY = scrolled;
-    });
+    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    let dY = scrolled - oldScrollY;
+
+    if (dY < 0) {
+      header.classList.add('header-fixed');
+    } else {
+      header.classList.remove('header-fixed');
+    }
+
+    hideHeader();
+    oldScrollY = scrolled;
+  });
 }
 
 export default fixedHeader;
@@ -34,30 +43,30 @@ export default fixedHeader;
 
 // Шапка липнет при скролле
 
-    // function headerScrollAnimation() {
-    //     const header = document.querySelector('.main-header');
+// function headerScrollAnimation() {
+//     const header = document.querySelector('.main-header');
 
-    //     function hideHeader() {
-    //         if (window.pageYOffset > header.offsetHeight) {
-    //             header.classList.add('header-hide');
-    //         } else {
-    //             header.classList.remove('header-hide');
-    //         }
-    //     }
+//     function hideHeader() {
+//         if (window.pageYOffset > header.offsetHeight) {
+//             header.classList.add('header-hide');
+//         } else {
+//             header.classList.remove('header-hide');
+//         }
+//     }
 
-    //     function fixedHeader() {
-    //         if (window.pageYOffset > header.offsetHeight * 2) {
-    //             header.classList.add('header-fixed');
-    //             document.body.style.marginTop = `${header.clientHeight}px`;
-    //         } else {
-    //             header.classList.remove('header-fixed');
-    //             document.body.style.marginTop = '0px';
-    //         }
-    //     }
+//     function fixedHeader() {
+//         if (window.pageYOffset > header.offsetHeight * 2) {
+//             header.classList.add('header-fixed');
+//             document.body.style.marginTop = `${header.clientHeight}px`;
+//         } else {
+//             header.classList.remove('header-fixed');
+//             document.body.style.marginTop = '0px';
+//         }
+//     }
 
-    //     hideHeader();
-    //     fixedHeader();
-    // }
+//     hideHeader();
+//     fixedHeader();
+// }
 
-    // headerScrollAnimation();
-    // window.addEventListener('scroll', headerScrollAnimation);
+// headerScrollAnimation();
+// window.addEventListener('scroll', headerScrollAnimation);
